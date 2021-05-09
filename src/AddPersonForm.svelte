baa3cfaffaf3681090fba7d6921a68f778e63a39
dyogi24 <yogiindrawan24@gmail.com>
2021-05-09 15:20:55 +0800

    day 1 learn

---
+++ src/AddPersonForm.svelte
@@ -0,0 +1,32 @@
+<script>
+  let name;
+  let beltColor;
+  let age;
+
+  let skills = [];
+
+  const handleSubmit = () => {
+    console.log(name, beltColor, age, skills);
+  };
+</script>
+
+<form on:submit|preventDefault={handleSubmit}>
+  <input type="text" placeholder="Name" bind:value={name} />
+  <input type="number" placeholder="Age" bind:value={age} />
+  <label>Skills:</label>
+  <input type="checkbox" bind:group={skills} value="fighting" />fighting<br />
+  <input type="checkbox" bind:group={skills} value="runing" />Runing<br />
+  <input type="checkbox" bind:group={skills} value="sneaking" />Sneaking<br />
+  <label>Belt Color:</label>
+  <select bind:value={beltColor}>
+    <option value="white">White</option>
+    <option value="yellow">Yellow</option>
+    <option value="green">Green</option>
+    <option value="blue">Blue</option>
+    <option value="brown">Brown</option>
+    <option value="black">Black</option>
+  </select>
+  <button>Add Person</button>
+</form>
+
+<style></style>
--- src/App.svelte
+++ src/App.svelte
@@ -1,30 +1,61 @@
 <script>
-	export let name;
+  import Modal from "./Modal.svelte";
+  import AddPersonForm from "./AddPersonForm.svelte";
+
+  let pepole = [
+    { name: "yoshi", beltColor: "Black", age: 30, id: 1 },
+    { name: "ken", beltColor: "Blue", age: 17, id: 2 },
+    { name: "Ikagura", beltColor: "Orange", age: 10, id: 3 },
+  ];
+
+  const deletePerson = (id) => {
+    pepole = pepole.filter((person) => person.id != id);
+  };
+
+  let showModal = true;
+  //   modal handler
+  const handleModal = () => {
+    showModal = !showModal;
+  };
 </script>
 
+<Modal {showModal} on:click={handleModal}>
+  <h3>Add person</h3>
+  <AddPersonForm />
+</Modal>
 <main>
-	<h1>Hello {name}!</h1>
-	<p>Visit the <a href="https://svelte.dev/tutorial">Svelte tutorial</a> to learn how to build Svelte apps.</p>
+  <button on:click={handleModal}>Open Modal</button>
+  {#each pepole as person (person.id)}
+    <div>
+      <h4>{person.name}</h4>
+      {#if person.beltColor === "Black"}
+        <p><strong>Master</strong></p>
+      {/if}
+      <p>{person.age} Year old, {person.beltColor} Belt.</p>
+      <button on:click={() => deletePerson(person.id)}>Delete</button>
+    </div>
+  {:else}
+    <div>No pepole to show.....</div>
+  {/each}
 </main>
 
 <style>
-	main {
-		text-align: center;
-		padding: 1em;
-		max-width: 240px;
-		margin: 0 auto;
-	}
+  main {
+    text-align: center;
+    padding: 1em;
+    max-width: 240px;
+    margin: 0 auto;
+  }
 
-	h1 {
-		color: #ff3e00;
-		text-transform: uppercase;
-		font-size: 4em;
-		font-weight: 100;
-	}
+  h4 {
+    text-transform: uppercase;
+    font-size: 2em;
+    font-weight: 100;
+  }
 
-	@media (min-width: 640px) {
-		main {
-			max-width: none;
-		}
-	}
+  @media (min-width: 640px) {
+    main {
+      max-width: none;
+    }
+  }
 </style>
